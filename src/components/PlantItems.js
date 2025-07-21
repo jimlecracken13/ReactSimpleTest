142a4f83f57c8eb7e8b347310399fd9ee0f18829
jimlecracken14 <jimlecracken13@gmail.com>
2025-07-21 17:04:46 +0000

    gestion d'evenement

--- src/components/PlantItems.js
+++ src/components/PlantItems.js
@@ -1,21 +1,21 @@
-import CareScale from './CareScale'
-import cover from '../assets/monstera.jpg'
-import '../styles/PlantItems.css'
-const PlantItems = ({plant})=>{
-    return(
-        <li key={plant.id} className='lmj-plant-item'>
-            {plant.name}
-            <img src={cover} alt="cover" className="lmj-plant-item-cover "/>
-            {    
-            plant.isSpecialOffer && (<div>
-                    soldes
-                  </div>)
-            }
-            <CareScale careType='water'
-            scaleValue={plant.water}/>
-            <CareScale careType='light'
-            scaleValue={plant.light}/>
-        </li>
-    )
+import CareScale from "./CareScale";
+import cover from "../assets/monstera.jpg";
+import "../styles/PlantItems.css";
+function handleClick(e)
+{
+    console.log("Ceci est mon event", e)
 }
-export default PlantItems
+const PlantItems = ({ plant }) => {
+  return (
+    <li key={plant.id} className="lmj-plant-item" onClick={handleClick}>
+      {plant.name}
+      <img src={cover} alt="cover" className="lmj-plant-item-cover " />
+      {plant.isSpecialOffer && <div>soldes</div>}
+      <div>
+        <CareScale careType="water" scaleValue={plant.water} />
+        <CareScale careType="light" scaleValue={plant.light} />
+      </div>
+    </li>
+  );
+};
+export default PlantItems;
